8c39185d19ded16c00638670a8d0485196d9f0b4
cho youngwoo <jovision00@naver.com>
2026-02-24 15:16:15 +0900

    fix console syntax error

--- src/page/Result.jsx
+++ src/page/Result.jsx
@@ -168,6 +168,12 @@ function Result() {
           {items.map((item) => (
             <ItemRow key={`buy-${item.id}`} item={item} showButtons={true} />
           ))}
+
+
+            <p className="affiliate-notice">
+    ※ 본 페이지는 쿠팡 파트너스 및 제휴 마케팅 활동의 일환으로,
+    이에 따른 일정액의 수수료를 제공받을 수 있습니다.
+  </p>
         </div>
 
       </div>
